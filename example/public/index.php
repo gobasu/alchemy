4ee5a75c59f9894bb48b0c53c03c521d9c91c42c
Dawid Kraczkowski <dawid@w3labs.pl>
2012-12-17 08:28:10 +0100

    removed old app folder and added hello world application

---
+++ example/public/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * YOUR BOOTSTRAP FILE
+ *
+ * If you would like to use other application namespace than example
+ * just change your application's root directory name and you namespaces
+ */
+require_once realpath(dirname(__FILE__) . '/../../alchemy/app/Application.php');
+
+use alchemy\app\Application;
+
+$app = new Application(realpath(dirname(__FILE__) . '/../'));
+$app->addRoute('*', 'example\controller\HelloWorld->sayHello'); //default route
+$app->run();
