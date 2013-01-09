792d0cf637a25e4ca7f392c8268c69b2b96f402c
Dawid Kraczkowski <dawid@w3labs.pl>
2013-01-09 12:47:01 +0100

    added composer.json+moved framework to src

--- example/public/index.php
+++ example/public/index.php
@@ -5,7 +5,7 @@
  * If you would like to use other application namespace than example
  * just change your application's root directory name and you namespaces
  */
-require_once realpath(dirname(__FILE__) . '/../../alchemy/app/Application.php');
+require_once realpath(dirname(__FILE__) . '/../../src/alchemy/app/Application.php');
 
 use alchemy\app\Application;
 
