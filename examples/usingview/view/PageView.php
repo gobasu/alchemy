ce11317cb4d172635da7568b6951e32d34f1b6b5
Dawid Kraczkowski <dawid@w3labs.pl>
2013-04-05 16:34:51 +0200

    view/template example fixed

--- examples/usingview/view/PageView.php
+++ examples/usingview/view/PageView.php
@@ -7,10 +7,21 @@
  * @license   https://raw.github.com/dkraczkowski/alchemy/master/LICENSE New BSD License
  */
 namespace usingview\view;
+use alchemy\future\template\renderer\Mixture;
+
 class PageView extends \alchemy\app\View
 {
     public function render()
     {
-
+        $context = array(
+            'some'  => array(
+                'var'   => array(
+                    'in'    => array(1,2,3,4,5)
+                )
+            ),
+            'title' => 'Sample title'
+        );
+        $renderer = new Mixture();
+        $renderer->render('child.html', $context);
     }
 }
